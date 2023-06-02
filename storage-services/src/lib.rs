2f6c2936aa7ffa47556e982f053275cd0a91626b
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2023-06-02 18:21:46 +0000

    update pallet

--- storage-services/src/lib.rs
+++ storage-services/src/lib.rs
@@ -1,7 +1,6 @@
-// Import the necessary dependencies
 use frame_support::{
     pallet_prelude::*,
-    traits::{Get, Currency},
+    traits::{Currency, Get},
 };
 use frame_system::pallet_prelude::*;
 
@@ -11,8 +10,27 @@ pub mod pallet {
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        // Define your configuration traits here
-        // Example: type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+
+        #[pallet::constant]
+        type MaxCapacity: Get<u64>;
+
+        #[pallet::constant]
+        type InitialReliability: Get<u64>;
+
+        #[pallet::constant]
+        type InitialReputation: Get<u64>;
+
+        type Balance: Currency<Self::AccountId>;
+    }
+
+    #[derive(Encode, Decode, Default, Clone, PartialEq)]
+    pub struct ProviderInfo<T: Config> {
+        pub capacity: u64,
+        pub reliability: u64,
+        pub reputation: u64,
+        pub rewards: T::Balance,
+        pub commissions: T::Balance,
     }
 
     #[pallet::pallet]
@@ -20,21 +38,26 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::storage]
-    #[pallet::getter(fn my_storage_item)]
-    pub type MyStorageItem<T> = StorageValue<_, u32>;
+    #[pallet::getter(fn storage_providers)]
+    pub type StorageProviders<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::AccountId, ProviderInfo<T>>;
 
     #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
     #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        // Define your events here
-        // Example: MyEvent(u32, T::AccountId),
+        ProviderRegistered(T::AccountId),
+        ProviderUpdated(T::AccountId),
+        ProviderRemoved(T::AccountId),
     }
 
     #[pallet::error]
     pub enum Error<T> {
-        // Define your errors here
-        // Example: MyError,
+        ProviderAlreadyRegistered,
+        ProviderNotRegistered,
+        CapacityOverflow,
+        InvalidReliabilityValue,
+        InvalidReputationValue,
     }
 
     #[pallet::hooks]
@@ -42,13 +65,87 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        // Define the dispatchable functions for your pallet
-        // Example: #[pallet::weight(10_000)]
-        //          pub fn my_function(origin: OriginFor<T>, value: u32) -> DispatchResultWithPostInfo {
-        //              let who = ensure_signed(origin)?;
-        //              MyStorageItem::<T>::put(value);
-        //              Self::deposit_event(Event::MyEvent(value, who));
-        //              Ok(().into())
-        //          }
+        #[pallet::weight(10_000)]
+        pub fn register_provider(
+            origin: OriginFor<T>,
+        ) -> DispatchResultWithPostInfo {
+            let provider = ensure_signed(origin)?;
+
+            ensure!(
+                !<StorageProviders<T>>::contains_key(&provider),
+                Error::<T>::ProviderAlreadyRegistered
+            );
+
+            let provider_info = ProviderInfo {
+                capacity: T::MaxCapacity::get(),
+                reliability: T::InitialReliability::get(),
+                reputation: T::InitialReputation::get(),
+                rewards: T::Balance::zero(),
+                commissions: T::Balance::zero(),
+            };
+
+            <StorageProviders<T>>::insert(&provider, provider_info);
+            Self::deposit_event(Event::ProviderRegistered(provider));
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn update_provider(
+            origin: OriginFor<T>,
+            capacity: u64,
+            reliability: u64,
+            reputation: u64,
+        ) -> DispatchResultWithPostInfo {
+            let provider = ensure_signed(origin)?;
+
+            let mut provider_info = Self::storage_providers(&provider)
+                .ok_or(Error::<T>::ProviderNotRegistered)?;
+
+            provider_info.capacity = capacity;
+            provider_info.reliability = reliability;
+            provider_info.reputation = reputation;
+
+            <StorageProviders<T>>::insert(&provider, provider_info.clone());
+            Self::deposit_event(Event::ProviderUpdated(provider));
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        pub fn remove_provider
+        origin: OriginFor<T>,
+    ) -> DispatchResultWithPostInfo {
+        let provider = ensure_signed(origin)?;
+
+        ensure!(
+            <StorageProviders<T>>::contains_key(&provider),
+            Error::<T>::ProviderNotRegistered
+        );
+
+        <StorageProviders<T>>::remove(&provider);
+        Self::deposit_event(Event::ProviderRemoved(provider));
+        Ok(().into())
     }
 }
+
+impl<T: Config> Pallet<T> {
+    fn validate_reliability(reliability: u64) -> Result<(), Error<T>> {
+        // Perform your reliability validation logic here
+        // For example, check if the value is within a valid range
+        if reliability > 100 {
+            Err(Error::<T>::InvalidReliabilityValue)
+        } else {
+            Ok(())
+        }
+    }
+
+    fn validate_reputation(reputation: u64) -> Result<(), Error<T>> {
+        // Perform your reputation validation logic here
+        // For example, check if the value is within a valid range
+        if reputation > 100 {
+            Err(Error::<T>::InvalidReputationValue)
+        } else {
+            Ok(())
+        }
+    }
+}
+
